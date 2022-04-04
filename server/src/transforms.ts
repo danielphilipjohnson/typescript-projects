1dc43c6e3dd22ed457bbed4b13fdf6dcf6b9c5ec
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-04-04 20:47:01 +0100

    added favorite transform

--- server/src/transforms.ts
+++ server/src/transforms.ts
@@ -1,12 +1,21 @@
-import { Tweet } from "./resolvers-types.generated"
-import { DbTweet } from "./db"
-export const tweetTransform = (
-  t: DbTweet
-): Omit<Tweet, "author"> => {
+import { Tweet, Favorite } from './resolvers-types.generated';
+import { DbTweet, DbFavorite } from './db';
+
+export const tweetTransform = (t: DbTweet): Omit<Tweet, 'author'> => {
   return {
     id: t.id,
     body: t.message,
     createdAt: t.createdAt,
     updatedAt: t.updatedAt,
-  }
-}
+  };
+};
+
+export const favoriteTransform = (
+  t: DbFavorite
+): Omit<Favorite, 'user' | 'tweet'> => {
+  return {
+    id: t.id,
+    createdAt: t.createdAt,
+    updatedAt: t.updatedAt,
+  };
+};
