8ccc03c9eb827c13ec9246ef11ca283cb4871a69
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-04-04 18:39:51 +0100

    Added tweet and user resolver

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,15 +1,22 @@
-import Db from "./db"
+import Db, { DbTweet, DbUser } from './db';
 
+import tweetTwitterResolver from './resolvers/Tweet';
+import userTwitterResolver from './resolvers/User';
 import { Resolvers } from './resolvers-types.generated';
 import queryTwitterResolvers from './resolvers/Query';
 
 export interface TwitterResolverContext {
-  db: Db
+  db: Db;
+  dbTweetCache: Record<string, DbTweet>;
+  dbUserCache: Record<string, DbUser>;
+  dbTweetToFavoriteCountMap: Record<string, number>;
 }
 
 export function createResolvers(): Resolvers<TwitterResolverContext> {
   const resolvers = {
     Query: queryTwitterResolvers,
+    Tweet: tweetTwitterResolver,
+    User: userTwitterResolver,
   };
 
   return resolvers;
