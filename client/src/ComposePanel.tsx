06f5a8111f963aef5d8b5e3c31c89d3916fa7454
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-04-04 20:13:17 +0100

    updated compose panel to make mutations

--- client/src/ComposePanel.tsx
+++ client/src/ComposePanel.tsx
@@ -7,22 +7,47 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 
+import { gql } from "@apollo/client"
+import { useCreateNewTweetMutation } from "./generated/graphql"
+import { GET_TIMELINE_TWEETS } from './Timeline';
+
+
+export const CREATE_NEW_TWEET = gql`
+  mutation CreateNewTweet(
+    $userId: String!
+    $body: String!
+  ) {
+    createTweet(userId: $userId, body: $body) {
+      id
+    }
+  }
+`
+
+
 export interface ComposePanelProps {
   currentUser: { id: string };
 }
 const ComposePanel: React.FC<ComposePanelProps> = ({ currentUser }) => {
-  function createNewTweet(body: string) {
-    console.log('creating new tweet', { body, currentUser });
-  }
+  const [createNewTweet, { error }] = useCreateNewTweetMutation()
+  if (error) return <p>Error creating new tweet: {error}</p>
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const textarea = e.currentTarget.querySelector('textarea');
     if (!textarea) throw new Error('No textarea found');
     const body = textarea.value;
-    createNewTweet(body);
+
+    createNewTweet({
+      variables: { userId: currentUser.id, body },
+      refetchQueries: [GET_TIMELINE_TWEETS],
+    }).then(() => {
+      textarea.value = '';
+    }).catch((err: unknown) => {
+        console.error('Problem creating new tweet', err);
+    });
     textarea.value = '';
   };
+
   return (
     <div className="new-tweet">
       <form onSubmit={handleSubmit}>
